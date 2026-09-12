3d3073a8dbd0c5a9be519f8e6cc3d781376a57bf
NamanKarwa17 <namankarwa17@gmail.com>
2026-09-12 15:51:02 +0000

    change name

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,12 @@
+const accountId = 12345
+let accountEmail = "naman@gmail"
+var accountPass = "@12345"
+accountCity = "Jodhpur"
+
+// dont use var in javascript because the issue is block scope and funcational scope
+
+accountEmail = "n@gmail.com"
+accountPass = "1234"
+accountCity = "jaipur"
+
+console.table([accountId,accountEmail,accountPass,accountCity])
