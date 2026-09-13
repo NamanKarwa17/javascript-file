105383fa3e680429811a8e44d6387c536d8115b9
NamanKarwa17 <namankarwa17@gmail.com>
2026-09-13 15:04:26 +0000

    number and maths

---
+++ 01_basic/number_maths.js
@@ -0,0 +1,11 @@
+const score = 100.9999
+console.log(score);
+
+const accountBalance = new Number(100.9999)
+console.log(accountBalance);
+
+console.log(accountBalance.toString().length);
+console.log(accountBalance.toFixed(2));
+console.log(accountBalance.toPrecision(3));
+
+
