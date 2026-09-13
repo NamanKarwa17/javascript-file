835cc090a848e526d13f605a2e39404ce94b6858
NamanKarwa17 <namankarwa17@gmail.com>
2026-09-13 14:21:48 +0000

    string change

--- 01_basic/string.js
+++ 01_basic/string.js
@@ -1,25 +1,25 @@
 const name = "Naman"
-// const repoCount = 30
+const repoCount = 30
 
-// // console.log(`hello my name is ${name} and my repo count is ${repoCount}`); //--STRING INTERPOLATION--
+console.log(`hello my name is ${name} and my repo count is ${repoCount}`); //--STRING INTERPOLATION--
 
 const gameName = new String('namank')
-// console.log(gameName.length);
-// console.log(gameName.toUpperCase());
-// console.log(gameName.toLowerCase());
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.toLowerCase());
 
-// console.log(gameName.substring(0,5));
+console.log(gameName.substring(0,5));
 
-// const wordSpace = "    RAGHAV    "
-// console.log(wordSpace.trim());
+const wordSpace = "    RAGHAV    "
+console.log(wordSpace.trim());
 
-// console.log(wordSpace.slice(0,5));
-// console.log(wordSpace.replace("    RAGHAV    ","ganga"));
+console.log(wordSpace.slice(0,5));
+console.log(wordSpace.replace("    RAGHAV    ","ganga"));
 
-// //split funcaion
-// const str = "The quick brown fox jumps over the lazy dog.";
-// const word = str.split(" ");
-// console.log(word[3]);
+//split funcaion
+const str = "The quick brown fox jumps over the lazy dog.";
+const word = str.split(" ");
+console.log(word[3]);
 
 console.log(name.bold());
 
